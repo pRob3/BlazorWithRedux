9549ead310f7598d9589f9c0a83b1615a88a4f45
pRob3 <bubblan@slaf.se>
2022-04-11 20:16:44 +0200

    WeatherForecast using Fluxor

---
+++ BlazorWithFluxor/Client/Features/Weather/Store/WeatherStore.cs
@@ -0,0 +1,111 @@
+﻿using BlazorWithFluxor.Shared;
+using Fluxor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWithFluxor.Client.Features.Weather.Store
+{
+    public record WeatherState
+    {
+        public bool Initialized { get; init; }
+        public bool Loading { get; init; }
+        public WeatherForecast[] Forecasts { get; init; }
+    }
+
+    // Feature
+    public class WeatherFeature : Feature<WeatherState>
+    {
+        public override string GetName() => "Weather";
+
+        protected override WeatherState GetInitialState()
+        {
+            return new WeatherState
+            {
+                Initialized = false,
+                Loading = false,
+                Forecasts = Array.Empty<WeatherForecast>()
+            };
+        }
+    }
+
+    // Actions
+    public class WeatherSetInitializedAction { }
+
+    public class WeatherSetForecastsAction
+    {
+        public WeatherForecast[] Forecasts { get; }
+
+        public WeatherSetForecastsAction(WeatherForecast[] forecasts)
+        {
+            Forecasts = forecasts;
+        }
+    }
+
+    public class WeatherSetLoadingAction
+    {
+        public bool Loading { get; }
+
+        public WeatherSetLoadingAction(bool loading)
+        {
+            Loading = loading;
+        }
+    }
+
+    public class WeatherLoadForecastsAction { }
+
+
+    // Reducers
+    public static class WeatherReducers
+    {
+        [ReducerMethod]
+        public static WeatherState OnSetForecasts(WeatherState state, WeatherSetForecastsAction action)
+        {
+            return state with
+            {
+                Forecasts = action.Forecasts
+            };
+        }
+
+        [ReducerMethod]
+        public static WeatherState OnSetLoading(WeatherState state, WeatherSetLoadingAction action)
+        {
+            return state with
+            {
+                Loading = action.Loading
+            };
+        }
+
+        [ReducerMethod(typeof(WeatherSetInitializedAction))]
+        public static WeatherState OnSetInitialized(WeatherState state)
+        {
+            return state with
+            {
+                Initialized = true
+            };
+        }
+    }
+
+    // Effects
+    public class WeatherEffects
+    {
+        private readonly HttpClient Http;
+
+        public WeatherEffects(HttpClient http)
+        {
+            Http = http;
+        }
+
+        [EffectMethod(typeof(WeatherLoadForecastsAction))]
+        public async Task LoadForecasts(IDispatcher dispatcher)
+        {
+            dispatcher.Dispatch(new WeatherSetLoadingAction(true));
+            var forecasts = await Http.GetFromJsonAsync<WeatherForecast[]>("WeatherForecast");
+            dispatcher.Dispatch(new WeatherSetForecastsAction(forecasts));
+            dispatcher.Dispatch(new WeatherSetLoadingAction(false));
+        }
+    }
+}
--- BlazorWithFluxor/Server/Controllers/WeatherForecastController.cs
+++ BlazorWithFluxor/Server/Controllers/WeatherForecastController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorWithFluxor.Server.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
