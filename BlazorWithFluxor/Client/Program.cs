82a44b1c34b0c2bf09a6f639308da5167bc39ab7
pRob3 <bubblan@slaf.se>
2022-04-11 18:49:17 +0200

    Core hosted with individual accounts

---
+++ BlazorWithFluxor/Client/Program.cs
@@ -0,0 +1,29 @@
+using BlazorWithFluxor.Client;
+using Fluxor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddHttpClient("BlazorWithFluxor.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+// Supply HttpClient instances that include access tokens when making requests to the server project
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BlazorWithFluxor.ServerAPI"));
+
+builder.Services.AddApiAuthorization();
+
+// Add Fluxor
+builder.Services.AddFluxor(options =>
+{
+    options
+        .ScanAssemblies(typeof(Program).Assembly)
+        // For ReduxDevTool support. Don't have this in Production
+        .UseReduxDevTools();
+});
+
+
+await builder.Build().RunAsync();
---
+++ BlazorWithFluxor/Server/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BlazorWithFluxor.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
